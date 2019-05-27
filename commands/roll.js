2f2ee4e94c4c987cb7278c5b9350597e215db73e
SPak12 <seantwelve3@gmail.com>
2019-05-26 22:21:25 -0700

    Fixed roll 9-digit number

--- commands/roll.js
+++ commands/roll.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'roll',
     execute(message, args) {
-        if (args.length > 0 && Number.isInteger(args[0]) && args[0].length < 10) {
-            var randomNumber = Math.floor(Math.random() * parseInt(args[0]));
+        if (args.length > 0 && !isNaN(args[0]) && args[0].length < 10) {
+            var randomNumber = Math.floor(Math.random() * (parseInt(args[0]) + 1));
         } else {
             var randomNumber = Math.floor(Math.random() * 101);
         }

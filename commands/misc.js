f3c01e804b16d9a6900bba8b053668272f0f2f9e
SPak12 <seantwelve3@gmail.com>
2019-09-07 18:08:39 -0700

    Included apcp

--- commands/misc.js
+++ commands/misc.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'misc',
     execute(message, args) {
         const msg = message.content.toLowerCase().replace(/\s/g, '');
-        if (msg.includes('potassiumnitrate') || msg.includes('bomb') || msg.includes('ammoniumperchlorate')) {
+        if (msg.includes('potassiumnitrate') || msg.includes('bomb') || msg.includes('ammoniumperchlorate') || msg.includes('apcp')) {
             message.channel.send('You have been added to the FBI watchlist.');
         } else if (msg.includes('motor')) {
             message.channel.send('whoosh');
